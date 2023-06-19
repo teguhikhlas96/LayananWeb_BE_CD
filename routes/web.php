a8f5918af1f96d0e9ef82fff8aed46e67c13c71e
Teguh Ikhlas Ramadhan <gamanspin48@gmail.com>
2023-06-19 10:19:33 +0700

    add get user api

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,5 @@ $router->get('/', function () use ($router) {
 $router->post('/register', 'AuthController@register');
 $router->post('/login', 'AuthController@login');
 $router->get('/user', ['middleware' => 'auth', 'uses' => 'AuthController@user']);
+$router->get('/users', ['middleware' => 'auth', 'uses' => 'AuthController@getUserData']);
 
